3a7f4ef38c5dd09dacc3539c5ed97ec22451f734
Ashok Amara <ashok.amara@cesneosilica.com>
2024-04-03 13:42:29 +0530

    Volume tracker App completed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
     <h1> Volume Tracker Application in VueJs</h1>
+    <h2 class="volumeHeading">Current Volume:<span> {{ volume }}</span></h2>
+    <button @click="volume += 2" class="increase">Increase</button>
+    <button @click="volume-= 2" class="decrease">Decrease</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      volume:0
+    }
+  },
+  methods:{},
+  computed:{},
+  watch:{
+    volume(newValue, oldValue){
+      if(newValue > oldValue && newValue === 16){
+        alert("it is High Volume, Not Recommended")
+      }
+      if( newValue < oldValue && oldValue === 4){
+        alert("it is Low Volume")
+      }
+    }
+  }
 }
 </script>
 
@@ -18,5 +38,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: tan;
+  width: 50%;
+  margin:0 auto;
+  height:200px;
+  line-height: 45px;
+  
 }
+.increase{width:100px;background-color: cadetblue;height: 40px;border: 2px solid cadetblue;border-radius: 50%;}
+.decrease{width: 100px;background-color: tomato;height: 40px;border:2px solid tomato;border-radius: 50%;}
+.volumeHeading{color: steelblue;}
+span{color: snow;}
 </style>
